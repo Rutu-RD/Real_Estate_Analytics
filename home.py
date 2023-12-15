a0a5927d2ae4786e3e1a79a28b6dfb1ed756009a
ruturaj desai <desairuturaj98@gmail.com>
2023-12-15 02:00:31 -0500

    added EDA Univariate and added houses info

---
+++ home.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.write("# Real Estate Market")
+st.sidebar.success("Select a demo above.")
---
+++ pages/Price Prediction.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.set_page_config(page_title="Price Prediction")
+st.title('Prediction of house price ')
